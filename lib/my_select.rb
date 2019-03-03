52ce17a72decc900c79f54cd4f13fdb2b44b9edb
Jesse Smokovich <jessesmokovich@yahoo.com>
2019-03-03 02:35:33 +0000

    Done.

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -1,3 +1,18 @@
-def my_select(collection)
- # your code here!
+def my_select(collection) 
+  selection = []
+  i = 0
+
+  while i < collection.length
+    if 
+    yield(collection[i]) == true
+    selection.push(collection[i])
+    end
+    i += 1
+  end
+  return selection
 end
+
+
+
+
+ 
